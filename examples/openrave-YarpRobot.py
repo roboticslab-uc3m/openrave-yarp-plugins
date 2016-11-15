6dd43b9851647192234ca7de9b14c3ad6f60d85b
RaulFdzbis <raulfernandezbis@gmail.com>
2016-11-15 17:14:36 +0100

    Closes #7

--- examples/openrave-YarpRobot.py
+++ examples/openrave-YarpRobot.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+import openravepy
 from openravepy import *
 RaveInitialize()
 
 if not RaveLoadPlugin('./OpenraveYarpControlboard'):
     raveLogError("Plugin not correctly loaded")
-    
+   
 try:
     env=Environment()
-    env.Load('robots/barrettwam.robot.xml')
+    env.SetViewer('qtcoin')
+    env.Load('/home/raul/repos/teo-main/share/openrave/teo/teo.robot.xml')
+    teo_robot = env.GetRobots()[0]
+
     YarpRobot = RaveCreateModule(env,'OpenraveYarpControlboard')
+
     print YarpRobot.SendCommand('open')
     while 1:
         pass
+
 finally:
     RaveDestroy()
+
