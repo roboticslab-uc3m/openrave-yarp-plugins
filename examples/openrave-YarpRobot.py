4b410db902c4effb5ab83fb5afb7af5d5e4812aa
RaulFdzbis <raulfernandezbis@gmail.com>
2016-11-16 12:02:50 +0100

    Closes #8

--- examples/openrave-YarpRobot.py
+++ examples/openrave-YarpRobot.py
@@ -12,9 +12,28 @@ try:
     env.Load('/home/raul/repos/teo-main/share/openrave/teo/teo.robot.xml')
     teo_robot = env.GetRobots()[0]
 
-    YarpRobot = RaveCreateModule(env,'OpenraveYarpControlboard')
+    OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpControlboard')
+    print OpenraveYarpControlboard.SendCommand('open')
+	
+    # Convex Decomposition padding
+    cdmodel = databases.convexdecomposition.ConvexDecompositionModel(teo_robot)
+    
+    # Load the ConvexDecomposition model, if it does not exit in the database generate it.
+    if not cdmodel.load(): # It always load the model with zero padding
+        # If not already in the database. Generate:
+    	cdmodel.generate(padding=0.02)
+    
+    cdmodel.save()
+    print 'Finished saving'
 
-    print YarpRobot.SendCommand('open')
+    print 'Setting robot...'
+    cdmodel.setrobot()
+    print 'Finish setrobot'
+    
+    print 'Starting visualization of the convexdecomposition model'
+    cdmodel.show()
+    print 'Finished loading visualization'
+
     while 1:
         pass
 
