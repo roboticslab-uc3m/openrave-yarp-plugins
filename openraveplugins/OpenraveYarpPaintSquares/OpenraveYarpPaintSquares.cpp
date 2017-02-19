c80724e19611ceac4c09ef0739d45dc24bb21c85
Raulfdzbis <rauferna@ing.uc3m.es>
2017-02-19 12:05:29 +0100

    remove unused code

--- openraveplugins/OpenraveYarpPaintSquares/OpenraveYarpPaintSquares.cpp
+++ openraveplugins/OpenraveYarpPaintSquares/OpenraveYarpPaintSquares.cpp
@@ -47,11 +47,6 @@ public:
     }
 
     virtual ~OpenraveYarpPaintSquares() {
-        for(int i=0;i<robotDevices.size();i++)
-        {
-            robotDevices[i]->close();
-            delete robotDevices[i];
-        }
     }
 
     virtual void Destroy() {
@@ -66,42 +61,6 @@ public:
 
     bool Open(ostream& sout, istream& sinput)
     {
-        bool collision = false;
-        bool alternativeRobotName = false;
-
-        vector<string> funcionArgs;
-        while(sinput)
-        {
-            string funcionArg;
-            sinput >> funcionArg;
-            funcionArgs.push_back(funcionArg);
-        }
-
-        if (funcionArgs.size() > 0)
-        {
-            if (funcionArgs[0] == "collision")
-            {
-                RAVELOG_INFO("Will open YarpOpenraveControlboardCollision");
-                collision = true;
-                if (funcionArgs.size() > 1)
-                {
-                    if( funcionArgs[1][0] == '/')
-                    {
-                        RAVELOG_INFO("Will use alternativeRobotName: %s",funcionArgs[1].c_str());
-                        alternativeRobotName = true;
-                    }
-                    else
-                    {
-                        RAVELOG_INFO("Will not use alternativeRobotName that does not begin with '/': %s",funcionArgs[1].c_str());
-                    }
-                }
-            }
-        }
-        else
-        {
-            RAVELOG_INFO("Will open YarpOpenraveControlboard");
-        }
-
         if ( !yarp.checkNetwork() )
         {
             RAVELOG_INFO("Found no yarp network (try running \"yarpserver &\"), bye!\n");
@@ -111,76 +70,11 @@ public:
         RAVELOG_INFO("penv: %p\n",GetEnv().get());
         OpenRAVE::EnvironmentBasePtr penv = GetEnv();
 
-        //-- Get robots
-        std::vector<OpenRAVE::RobotBasePtr> vectorOfRobotPtr;
-        GetEnv()->GetRobots(vectorOfRobotPtr);
-
-        //-- For each robot
-        for(size_t robotPtrIdx=0;robotPtrIdx<vectorOfRobotPtr.size();robotPtrIdx++)
-        {
-            RAVELOG_INFO( "Robots[%zu]: %s\n",robotPtrIdx,vectorOfRobotPtr[robotPtrIdx]->GetName().c_str());
-
-            //-- Get manipulators
-            std::vector<OpenRAVE::RobotBase::ManipulatorPtr> vectorOfManipulatorPtr = vectorOfRobotPtr[robotPtrIdx]->GetManipulators();
-
-            //-- For each manipulator
-            for(size_t manipulatorPtrIdx=0;manipulatorPtrIdx<vectorOfManipulatorPtr.size();manipulatorPtrIdx++)
-            {
-                RAVELOG_INFO( "* Manipulators[%zu]: %s\n",manipulatorPtrIdx,vectorOfManipulatorPtr[manipulatorPtrIdx]->GetName().c_str() );
-
-                //-- Formulate the manipulator port name
-                std::string manipulatorPortName;
-                if(alternativeRobotName)
-                {
-                    manipulatorPortName += funcionArgs[1];
-                }
-                else
-                {
-                    manipulatorPortName += "/";
-                    manipulatorPortName += vectorOfRobotPtr[robotPtrIdx]->GetName();
-                }
-                manipulatorPortName += "/";
-                manipulatorPortName += vectorOfManipulatorPtr[manipulatorPtrIdx]->GetName();
-                RAVELOG_INFO( "* manipulatorPortName: %s\n",manipulatorPortName.c_str() );
-
-                yarp::dev::PolyDriver* robotDevice = new yarp::dev::PolyDriver;
-                yarp::os::Property options;
-                options.put("device","controlboardwrapper2");  //-- ports
-
-                if (collision)
-                {
-                    options.put("subdevice","YarpOpenraveControlboardCollision");
-                    std::string safe("/safe");
-                    options.put("name", safe+manipulatorPortName );
-                    options.put("remote", manipulatorPortName );
-                }
-                else
-                {
-                    options.put("subdevice","YarpOpenraveControlboard");
-                    options.put("name", manipulatorPortName );
-                }
-
-                yarp::os::Value v(&penv, sizeof(OpenRAVE::EnvironmentBasePtr));
-                options.put("penv",v);
-
-                options.put("robotIndex",static_cast<int>(robotPtrIdx));
-                options.put("manipulatorIndex",static_cast<int>(manipulatorPtrIdx));
-
-                robotDevice->open(options);
-                if( ! robotDevice->isValid() )
-                {
-                    RAVELOG_INFO("Bad\n");
-                    return false;
-                }
-                robotDevices.push_back( robotDevice );
-            }
-        }
         return true;
     }
 
 private:
     yarp::os::Network yarp;
-    std::vector< yarp::dev::PolyDriver* > robotDevices;
 };
 
 InterfaceBasePtr CreateInterfaceValidated(InterfaceType type, const std::string& interfacename, std::istream& sinput, EnvironmentBasePtr penv) {
