dca81a6b3f32f64c90c9012d7fc25dc3a9ef7f7d
Raulfdzbis <rauferna@ing.uc3m.es>
2017-02-19 12:12:46 +0100

    init openrave-PaintSquares.py

---
+++ examples/openrave-PaintSquares.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import openravepy
+from openravepy import *
+
+try:
+    RaveInitialize()
+
+    if not RaveLoadPlugin('OpenraveYarpControlboard'):
+        raveLogError("Plugin not correctly loaded")
+
+    env=Environment()
+    env.SetViewer('qtcoin')
+    env.Load('/usr/local/share/teo-openrave-models/contexts/openrave/teo/teo.robot.xml')
+
+    OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpControlboard')
+    print OpenraveYarpControlboard.SendCommand('open')
+
+    while 1:
+        pass
+
+finally:
+    RaveDestroy()
+
