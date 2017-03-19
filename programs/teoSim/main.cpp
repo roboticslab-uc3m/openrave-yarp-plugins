87bd9864020aba9e990d9707fb58c593a66af463
jgvictores <jcgvicto@gmail.com>
2017-03-19 21:27:47 +0100

    kinematics-dynamics-programs -> openrave-yarp-plugins_programs

--- programs/teoSim/main.cpp
+++ programs/teoSim/main.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /**
- * @ingroup kinematics-dynamics-programs
+ * @ingroup openrave-yarp-plugins_programs
  *
  * \defgroup teoSim teoSim
  *
