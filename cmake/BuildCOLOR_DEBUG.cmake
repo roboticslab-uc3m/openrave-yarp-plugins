c4d034feed12e7ad010cebb38ca468e1d1054aca
Bartek Łukawski <bwmn.peter@gmail.com>
2017-08-11 19:01:38 +0200

    Rely on dummy library target created by color-debug's config file
    https://github.com/roboticslab-uc3m/color-debug/issues/6

--- cmake/BuildCOLOR_DEBUG.cmake
+++ cmake/BuildCOLOR_DEBUG.cmake
@@ -9,12 +9,10 @@ ycm_ep_helper(COLOR_DEBUG TYPE GIT
 ExternalProject_Get_Property(COLOR_DEBUG INSTALL_DIR)
 include_directories(${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
 
+# CMake has not downloaded color-debug yet (this happens on build step).
 if(NOT COLOR_DEBUG_FOUND)
     message(STATUS "Build COLOR_DEBUG target and configure project again to make advanced CD options available on UI.")
-endif()
-
-# CMake has downloaded color-debug (build step).
-if(COLOR_DEBUG_FOUND)
+else()
     # Load COLOR_DEBUGConfig.cmake, which in turn includes ColorDebugOptions.cmake.
     find_package(COLOR_DEBUG QUIET)
 endif()
