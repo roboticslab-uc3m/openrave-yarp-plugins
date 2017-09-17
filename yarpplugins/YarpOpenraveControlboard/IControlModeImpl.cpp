6aac150ca03267084b78018e04243e708be7a54b
jgvictores <jcgvicto@gmail.com>
2017-09-18 00:02:20 +0200

    begin controlmodes2

--- yarpplugins/YarpOpenraveControlboard/IControlModeImpl.cpp
+++ yarpplugins/YarpOpenraveControlboard/IControlModeImpl.cpp
@@ -2,11 +2,11 @@
 
 #include "YarpOpenraveControlboard.hpp"
 
-// ------------------- IControlLimits Related ------------------------------------
+// ------------------- IControlMode Related ------------------------------------
 
 bool roboticslab::YarpOpenraveControlboard::setPositionMode(int j) {
     CD_INFO("(%d)\n",j);
-    return true;
+    return setControlMode(j, VOCAB_CM_POSITION);
 }
 
 // -----------------------------------------------------------------------------
@@ -59,6 +59,53 @@ bool roboticslab::YarpOpenraveControlboard::getControlModes(int *modes) {
     bool ok = true;
     for(unsigned int i=0; i < axes; i++)
         ok &= getControlMode(i,&(modes[i]));
+    return ok;
+}
+
+// ---------------------- IControlMode2 Related  ---------------------------------
+
+bool roboticslab::YarpOpenraveControlboard::getControlModes(const int n_joint, const int *joints, int *modes)
+{
+    CD_DEBUG("\n");
+
+    bool ok = true;
+    for(unsigned int i=0; i < n_joint; i++)
+        ok &= getControlMode(joints[i],&modes[i]);
+    return ok;
+}
+
+// -----------------------------------------------------------------------------
+
+bool roboticslab::YarpOpenraveControlboard::setControlMode(const int j, const int mode)
+{
+    CD_DEBUG("(%d, %d)\n",j,mode);
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool roboticslab::YarpOpenraveControlboard::setControlModes(const int n_joint, const int *joints, int *modes)
+{
+    CD_DEBUG("(%d)\n",n_joint);
+
+    bool ok = true;
+    for(int j=0; j<n_joint; j++)
+    {
+        ok &= this->setControlMode(joints[j],modes[j]);
+    }
+    return ok;
+}
+
+// -----------------------------------------------------------------------------
+
+bool roboticslab::YarpOpenraveControlboard::setControlModes(int *modes)
+{
+    CD_DEBUG("\n");
+
+    bool ok = true;
+    for(unsigned int i=0; i<axes; i++)
+        ok &= setControlMode(i,modes[i]);
     return ok;
 }
 
