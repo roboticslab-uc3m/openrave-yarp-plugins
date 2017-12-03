0589735917cfa5127b4c869901e2f1838acaaad1
jgvictores <jcgvicto@gmail.com>
2017-12-03 17:16:32 +0100

    add parsable values

--- examples/openrave-YarpPluginLoader.py
+++ examples/openrave-YarpPluginLoader.py
@@ -14,7 +14,7 @@ try:
     env.Load('/usr/local/share/teo-openrave-models/contexts/openrave/teo/teo.robot.xml')
 
     OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpPluginLoader')
-    print OpenraveYarpControlboard.SendCommand('open')
+    print OpenraveYarpControlboard.SendCommand('open --key1 value1 --key2 value2')
 
     while 1:
         pass
