45dec74c520c34a83f56f35107fe22380f54b69b
jgvictores <jcgvicto@gmail.com>
2017-12-04 18:13:39 +0100

    load new YarpOpenraveGrabber

--- examples/openrave-YarpPluginLoader-grabber.py
+++ examples/openrave-YarpPluginLoader-grabber.py
@@ -14,7 +14,7 @@ try:
     env.Load('/usr/local/share/robotDevastation-openrave-models/contexts/openrave/ecro/mapping_room.env.xml')
 
     OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
-    print OpenraveYarpPluginLoader.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 0 --prefix /drl')
+    print OpenraveYarpPluginLoader.SendCommand('open --device YarpOpenraveGrabber --robotIndex 0 --sensorIndex 0 --prefix /drl')
 
     while 1:
         pass
