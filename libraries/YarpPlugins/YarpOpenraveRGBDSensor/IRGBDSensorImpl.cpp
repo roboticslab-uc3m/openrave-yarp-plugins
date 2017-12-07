6f88ad086773d0e1ca36653772b4584cb530befe
jgvictores <jcgvicto@gmail.com>
2017-12-07 12:19:24 +0100

    implement depth

--- libraries/YarpPlugins/YarpOpenraveRGBDSensor/IRGBDSensorImpl.cpp
+++ libraries/YarpPlugins/YarpOpenraveRGBDSensor/IRGBDSensorImpl.cpp
@@ -11,33 +11,32 @@ bool YarpOpenraveRGBDSensor::getDepthImage(yarp::sig::ImageOf<yarp::sig::PixelFl
 {
     sensorBasePtr->GetSensorData(sensorDataPtr);
 
-    //std::vector< OpenRAVE::RaveVector< OpenRAVE::dReal > > sensorRanges = ptrVectorOfLaserSensorDataPtr->at(laserIter)->ranges;
-    //std::vector< OpenRAVE::RaveVector< OpenRAVE::dReal > > sensorPositions = ptrVectorOfLaserSensorDataPtr->at(laserIter)->positions;
-    //OpenRAVE::Transform tinv = ptrVectorOfLaserSensorDataPtr->at(laserIter)->__trans.inverse();
+    std::vector< OpenRAVE::RaveVector< OpenRAVE::dReal > > sensorRanges = sensorDataPtr->ranges;
+    std::vector< OpenRAVE::RaveVector< OpenRAVE::dReal > > sensorPositions = sensorDataPtr->positions;
+    OpenRAVE::Transform tinv = sensorDataPtr->__trans.inverse();
 
-    /*
-        if(sensorRanges.size()==3072) i_depth.resize(64,48);  // Tamaño de la pantalla (64,48)
-        else if(sensorRanges.size()==12288) i_depth.resize(128,96);
-        else if(sensorRanges.size()==49152) i_depth.resize(256,192);
-        else if(sensorRanges.size()==307200) i_depth.resize(640,480);
-        else if(sensorRanges.size()==4) i_depth.resize(2,2);
-        //else printf("[warning] unrecognized laser sensor data size.\n");
-        else i_depth.resize(sensorRanges.size(),1);
-        for (int i_y = 0; i_y < i_depth.height(); ++i_y) {  // was y in x before
-            for (int i_x = 0; i_x < i_depth.width(); ++i_x) {
-                //double p = sensorRanges[i_y+(i_x*i_depth.height())].z;
-                double p;
-                if( sensorPositions.size() > 0 ) {
-                    OpenRAVE::Vector v = tinv*(sensorRanges[i_y+(i_x*i_depth.height())] + sensorPositions[0]);
-                    p = (float)v.z;
-                } else {
-                    OpenRAVE::Vector v = tinv*(sensorRanges[i_y+(i_x*i_depth.height())]);
-                    p = (float)v.z;
-                }
-                i_depth(i_x,i_y) = p*1000.0;  // give mm
+    if(sensorRanges.size()==3072) depthImage.resize(64,48);  // Tamaño de la pantalla (64,48)
+    else if(sensorRanges.size()==12288) depthImage.resize(128,96);
+    else if(sensorRanges.size()==49152) depthImage.resize(256,192);
+    else if(sensorRanges.size()==307200) depthImage.resize(640,480);
+    else if(sensorRanges.size()==4) depthImage.resize(2,2);
+    //else printf("[warning] unrecognized laser sensor data size.\n");
+    else depthImage.resize(sensorRanges.size(),1);
+    for (int i_y = 0; i_y < depthImage.height(); ++i_y) {  // was y in x before
+        for (int i_x = 0; i_x < depthImage.width(); ++i_x) {
+            //double p = sensorRanges[i_y+(i_x*depthImage.height())].z;
+            double p;
+            if( sensorPositions.size() > 0 ) {
+                OpenRAVE::Vector v = tinv*(sensorRanges[i_y+(i_x*depthImage.height())] + sensorPositions[0]);
+                p = (float)v.z;
+            } else {
+                OpenRAVE::Vector v = tinv*(sensorRanges[i_y+(i_x*depthImage.height())]);
+                p = (float)v.z;
             }
+            depthImage(i_x,i_y) = p*1000.0;  // give mm
         }
-*/
+    }
+
     return true;
 }
 
