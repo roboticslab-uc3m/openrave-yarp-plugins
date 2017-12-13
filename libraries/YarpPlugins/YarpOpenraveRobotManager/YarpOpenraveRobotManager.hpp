e4080f5de13844902b791b600a80ab55aef31b5a
jgvictores <jcgvicto@gmail.com>
2017-12-13 21:53:47 +0100

    fix headers

--- libraries/YarpPlugins/YarpOpenraveRobotManager/YarpOpenraveRobotManager.hpp
+++ libraries/YarpPlugins/YarpOpenraveRobotManager/YarpOpenraveRobotManager.hpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __YARP_OPENRAVE_CONTROLBOARD_HPP__
-#define __YARP_OPENRAVE_CONTROLBOARD_HPP__
+#ifndef __YARP_OPENRAVE_ROBOT_MANAGER_HPP__
+#define __YARP_OPENRAVE_ROBOT_MANAGER_HPP__
 
 #include <yarp/os/all.h>
 #include <yarp/dev/all.h>
@@ -117,4 +117,4 @@ private:
 
 }  // namespace roboticslab
 
-#endif  // __YARP_OPENRAVE_CONTROLBOARD_HPP__
+#endif  // __YARP_OPENRAVE_ROBOT_MANAGER_HPP__
