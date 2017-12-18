97e5643902654769948feeec8fddb5c24c60933d
jgvictores <jcgvicto@gmail.com>
2017-12-18 14:58:04 +0100

    openrave-YarpRobotPaintSquares.py -> openraveYarpPaintSquares.py

--- examples/openraveYarpPaintSquares.py
+++ examples/openraveYarpPaintSquares.py
@@ -6,15 +6,22 @@ from openravepy import *
 try:
     RaveInitialize()
 
+    if not RaveLoadPlugin('OpenraveYarpPluginLoader'):
+        raveLogError("Plugin not correctly loaded")
+
     if not RaveLoadPlugin('OpenraveYarpPaintSquares'):
         raveLogError("Plugin not correctly loaded")
 
     env=Environment()
     env.SetViewer('qtcoin')
-    env.Load('/usr/local/share/xgnitive/contexts/models/teo_cgda_iros.env.xml')  #ugly way of loading the environment
+    env.Load('/usr/local/share/xgnitive/contexts/models/teo_cgda_iros.env.xml')  # assumes XGNITIVE installed
+
+    OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpControlboard.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 2 --genRefSpeed 999999')  # rightArm is maniplator 2
 
     OpenraveYarpPaintSquares = RaveCreateModule(env,'OpenraveYarpPaintSquares')
-    print OpenraveYarpPaintSquares.SendCommand('open')
+    print OpenraveYarpPaintSquares.SendCommand('open --squares 16')
+    #print OpenraveYarpPaintSquares.SendCommand('open --name /altPortName')
 
     while 1:
         pass
