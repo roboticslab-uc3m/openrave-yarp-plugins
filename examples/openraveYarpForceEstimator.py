b44ce749506ee1cb19d88efeb4f76a534d5eac9a
RaulFdzbis <raulfernandezbis@gmail.com>
2017-12-19 18:14:46 +0100

    Update openraveYarpForceEstimator.py

--- examples/openraveYarpForceEstimator.py
+++ examples/openraveYarpForceEstimator.py
@@ -6,6 +6,9 @@ from openravepy import *
 try:
     RaveInitialize()
 
+    if not RaveLoadPlugin('OpenraveYarpPluginLoader'):
+        raveLogError("Plugin not correctly loaded")
+        
     if not RaveLoadPlugin('OpenraveYarpForceEstimator'):
         raveLogError("Plugin not correctly loaded")
 
@@ -13,20 +16,13 @@ try:
     env.SetViewer('qtcoin')
     env.Load('/home/raul/repos/textiles/textiles/ironing/manipulation/ironingSim/ironingSim.env.xml')
 
-    #OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpControlboard')
-    #print OpenraveYarpControlboard.SendCommand('open')
-
-    #OpenraveWorldRpcResponder = RaveCreateModule(env,'OpenraveWorldRpcResponder')
-    #print OpenraveWorldRpcResponder.SendCommand('open')
-
-    #OpenraveYarpForceEstimator = RaveCreateModule(env,'OpenraveYarpForceEstimator')
-    #print OpenraveYarpForceEstimator.SendCommand('open')
-
     #New config:
 
-    OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpPluginLoader')
-    print OpenraveYarpControlboard.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 2 --genRefSpeed 999999')  # rightArm is maniplator 2
-    print OpenraveYarpControlboard.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 0 --genRefSpeed 999999')  # trunk is manipulator 0
+    OpenraveYarpControlboard0 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpControlboard0.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 2 --genRefSpeed 999999')  # rightArm is maniplator 2
+    
+    OpenraveYarpControlboard2 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpControlboard2.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 0 --genRefSpeed 999999')  # trunk is manipulator 0
     
     OpenraveYarpForceEstimator = RaveCreateModule(env,'OpenraveYarpForceEstimator')
     print OpenraveYarpForceEstimator.SendCommand('open')
