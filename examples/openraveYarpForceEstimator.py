d2b0ffe4a3e80ffbbe3a69dffea29164dc630057
Raulfdzbis <raulfernandezbis@gmail.com>
2017-12-19 17:51:45 +0100

    Updated openraveYarpForceEstimator to load plugin with params

--- examples/openraveYarpForceEstimator.py
+++ examples/openraveYarpForceEstimator.py
@@ -13,6 +13,21 @@ try:
     env.SetViewer('qtcoin')
     env.Load('/home/raul/repos/textiles/textiles/ironing/manipulation/ironingSim/ironingSim.env.xml')
 
+    #OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpControlboard')
+    #print OpenraveYarpControlboard.SendCommand('open')
+
+    #OpenraveWorldRpcResponder = RaveCreateModule(env,'OpenraveWorldRpcResponder')
+    #print OpenraveWorldRpcResponder.SendCommand('open')
+
+    #OpenraveYarpForceEstimator = RaveCreateModule(env,'OpenraveYarpForceEstimator')
+    #print OpenraveYarpForceEstimator.SendCommand('open')
+
+    #New config:
+
+    OpenraveYarpControlboard = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpControlboard.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 2 --genRefSpeed 999999')  # rightArm is maniplator 2
+    print OpenraveYarpControlboard.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboard --robotIndex 0 --manipulatorIndex 0 --genRefSpeed 999999')  # trunk is manipulator 0
+    
     OpenraveYarpForceEstimator = RaveCreateModule(env,'OpenraveYarpForceEstimator')
     print OpenraveYarpForceEstimator.SendCommand('open')
 
