c4f75f52d114acb2aec2364b7867a610a8d62a7e
jgvictores <jcgvicto@gmail.com>
2017-12-26 10:47:35 +0100

    drop prefix. no support for now

--- examples/openraveYarpPluginLoader-grabber.py
+++ examples/openraveYarpPluginLoader-grabber.py
@@ -14,7 +14,7 @@ try:
     env.Load('/usr/local/share/robotDevastation-openrave-models/contexts/openrave/ecro/mapping_room.env.xml')
 
     OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
-    print OpenraveYarpPluginLoader.SendCommand('open --device YarpOpenraveGrabber --robotIndex 0 --sensorIndex 1 --prefix /drl')
+    print OpenraveYarpPluginLoader.SendCommand('open --device YarpOpenraveGrabber --robotIndex 0 --sensorIndex 1')
 
     while 1:
         pass
