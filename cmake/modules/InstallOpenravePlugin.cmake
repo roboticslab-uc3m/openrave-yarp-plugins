4ad284c8650239be3d889c2aae8ee87c95d7a140
Bartek Łukawski <bwmn.peter@gmail.com>
2018-01-17 03:10:06 +0100

    Improve includes in OR plugins and CMake helpers

--- cmake/modules/InstallOpenravePlugin.cmake
+++ cmake/modules/InstallOpenravePlugin.cmake
@@ -1,15 +1,8 @@
-if(COMMAND build_openrave_plugin)
+if(COMMAND install_openrave_plugin)
     return()
 endif()
 
-macro(build_openrave_plugin name)
-    add_library(${name} SHARED ${name}.cpp)
-
-    set_target_properties(${name} PROPERTIES COMPILE_FLAGS "${OpenRAVE_CXX_FLAGS}"
-                                             LINK_FLAGS "${OpenRAVE_LINK_FLAGS}")
-
-    target_link_libraries(${name} ${YARP_LIBRARIES} ${OpenRAVE_LIBRARIES})
-
+macro(install_openrave_plugin target)
     execute_process(COMMAND openrave-config --prefix --plugins-dir
                     OUTPUT_VARIABLE _openrave_config_cmd_output
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -21,7 +14,7 @@ macro(build_openrave_plugin name)
         list(GET _openrave_config_results 0 _openrave_install_prefix)
         list(GET _openrave_config_results 1 _openrave_plugin_path)
         file(RELATIVE_PATH _relative_path "${_openrave_install_prefix}" "${_openrave_plugin_path}")
-        install(TARGETS ${name} DESTINATION ${_relative_path})
+        install(TARGETS ${target} DESTINATION ${_relative_path})
     else()
         message(AUTHOR_WARNING "Unsuccessful call to 'openrave-config', the output was: ${_openrave_config_cmd_output}")
     endif()
