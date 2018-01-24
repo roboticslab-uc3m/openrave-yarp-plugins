1cd29f363b10247fe5b62b9ebbde48e6dcecf7ca
jgvictores <jcgvicto@gmail.com>
2018-01-24 21:46:36 +0100

    support for two ecros
    - Matches https://github.com/asrob-uc3m/robotDevastation-openrave-models/commit/aca87c1129be9da2218a452c7a08ebd717e35f51

--- examples/openraveYarpPluginLoader-robotmanager.py
+++ examples/openraveYarpPluginLoader-robotmanager.py
@@ -13,11 +13,17 @@ try:
     env.SetViewer('qtcoin')
     env.Load('/usr/local/share/robotDevastation-openrave-models/contexts/openrave/ecro/mapping_room.env.xml')
 
-    OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    OpenraveYarpPluginLoader1 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpPluginLoader1.SendCommand('open --device RobotServer --subdevice YarpOpenraveRobotManager --robotIndex 0 --physics ode')
 
-    print OpenraveYarpPluginLoader.SendCommand('open --device RobotServer --subdevice YarpOpenraveRobotManager --robotIndex 0 --physics ode')
+    OpenraveYarpPluginLoader2 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpPluginLoader2.SendCommand('open --device grabber --subdevice YarpOpenraveGrabber --robotIndex 0 --sensorIndex 1 --name /ecroSim/img:o')
 
-    print OpenraveYarpPluginLoader.SendCommand('open --device grabber --subdevice YarpOpenraveGrabber --robotIndex 0 --sensorIndex 1 --name /ecroSim/img:o')
+    OpenraveYarpPluginLoader3 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpPluginLoader3.SendCommand('open --device RobotServer --subdevice YarpOpenraveRobotManager --robotIndex 1 --physics ode')
+
+    OpenraveYarpPluginLoader4 = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpPluginLoader4.SendCommand('open --device grabber --subdevice YarpOpenraveGrabber --robotIndex 1 --sensorIndex 1 --name /ecroSim2/img:o')
 
     #-- Model has 3d sensor, but not used yet.
     #print OpenraveYarpPluginLoader.SendCommand('open --device RGBDSensorWrapper --subdevice YarpOpenraveRGBDSensor --depthSensorIndex 0 --rgbSensorIndex 1')
