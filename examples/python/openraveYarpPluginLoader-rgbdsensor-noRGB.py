9ba6874c43f0dfedc258e26d3ab13ab4cbfaa899
Juan G. Victores <jgvictores@users.noreply.github.com>
2018-02-17 01:01:10 +0100

    Create openraveYarpPluginLoader-rgbdsensor-noRGB.py

---
+++ examples/python/openraveYarpPluginLoader-rgbdsensor-noRGB.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import openravepy
+from openravepy import *
+
+try:
+    RaveInitialize()
+
+    if not RaveLoadPlugin('OpenraveYarpPluginLoader'):
+        raveLogError("Plugin not correctly loaded")
+
+    env=Environment()
+    env.SetViewer('qtcoin')
+    env.Load('/usr/local/share/robotDevastation-openrave-models/contexts/openrave/ecro/mapping_room.env.xml')
+
+    OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    print OpenraveYarpPluginLoader.SendCommand('open --device RGBDSensorWrapper --subdevice YarpOpenraveRGBDSensor --robotIndex 0 --depthSensorIndex 0')
+
+    while 1:
+        pass
+
+finally:
+    RaveDestroy()
