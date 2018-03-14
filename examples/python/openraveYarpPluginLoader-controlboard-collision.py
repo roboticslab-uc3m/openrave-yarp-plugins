8524d39712663d011bf235b17ac1645043892ea6
jgvictores <jcgvicto@gmail.com>
2018-03-14 12:40:44 +0100

    comment out plugin for testing

--- examples/python/openraveYarpPluginLoader-controlboard-collision.py
+++ examples/python/openraveYarpPluginLoader-controlboard-collision.py
@@ -13,8 +13,8 @@ try:
     env.SetViewer('qtcoin')
     env.Load('/usr/local/share/teo-openrave-models/contexts/openrave/teo/teo.robot.xml')
 
-    OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
-    print OpenraveYarpPluginLoader.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboardCollision --robotIndex 0 --allManipulators')
+    #OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    #print OpenraveYarpPluginLoader.SendCommand('open --device controlboardwrapper2 --subdevice YarpOpenraveControlboardCollision --robotIndex 0 --allManipulators')
 
     # Convex Decomposition padding
     teo_robot = env.GetRobots()[0]
