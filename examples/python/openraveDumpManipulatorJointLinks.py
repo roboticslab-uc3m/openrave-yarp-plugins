6f83fe9d18f1d8e0dc9f8029850ba1d9da617fac
jgvictores <jcgvicto@gmail.com>
2019-12-15 18:05:53 +0100

    improve OpenraveDumpManipulatorJointLinks examples

--- examples/python/openraveDumpManipulatorJointLinks.py
+++ examples/python/openraveDumpManipulatorJointLinks.py
@@ -12,12 +12,7 @@ try:
     env=Environment()
     env.SetViewer('qtcoin')
 
-    example = "default"  # "default" or "teo"
-
-    if example == "teo":
-        env.Load('/usr/local/share/teo-openrave-models/openrave/teo/teo.robot.xml')
-    else:
-        env.Load('data/lab1.env.xml')
+    env.Load('data/lab1.env.xml')
 
     OpenraveDumpManipulatorJointLinks = RaveCreateModule(env,'OpenraveDumpManipulatorJointLinks')
     print OpenraveDumpManipulatorJointLinks.SendCommand('open')
---
+++ libraries/OpenravePlugins/OpenraveDumpManipulatorJointLinks/README.md
@@ -0,0 +1,9 @@
+# OpenraveDumpManipulatorJointLinks
+
+## Command Line (CLI) Invocation
+```bash
+openrave data/lab1.env.xml --module OpenraveDumpManipulatorJointLinks "open"
+```
+
+## Python Invocation
+The equivalent Python script can be found at: [../../../examples/python/openraveDumpManipulatorJointLinks.py](../../../examples/python/openraveDumpManipulatorJointLinks.py)
