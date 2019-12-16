d67fd2dcc887763421414e1af949017ab670394a
jgvictores <jcgvicto@gmail.com>
2019-12-16 21:24:38 +0100

    replace last of cam, now FT (single)

--- libraries/YarpPlugins/YarpOpenraveFTSensors/DeviceDriverImpl.cpp
+++ libraries/YarpPlugins/YarpOpenraveFTSensors/DeviceDriverImpl.cpp
@@ -63,7 +63,7 @@ bool YarpOpenraveFTSensors::open(yarp::os::Searchable& config)
     //sensorBasePtr->Configure(OpenRAVE::SensorBase::CC_RenderDataOn);
 
     // Get pointer to geom properties of sensor
-    boost::shared_ptr<OpenRAVE::SensorBase::CameraGeomData const> geomDataPtr = boost::dynamic_pointer_cast<OpenRAVE::SensorBase::CameraGeomData const>(sensorBasePtr->GetSensorGeometry(OpenRAVE::SensorBase::ST_Camera));
+    boost::shared_ptr<OpenRAVE::SensorBase::Force6DGeomData const> geomDataPtr = boost::dynamic_pointer_cast<OpenRAVE::SensorBase::Force6DGeomData const>(sensorBasePtr->GetSensorGeometry(OpenRAVE::SensorBase::ST_Force6D));
 
     // Get pointer to sensed data
     sensorDataPtr = boost::dynamic_pointer_cast<OpenRAVE::SensorBase::Force6DSensorData>(sensorBasePtr->CreateSensorData(OpenRAVE::SensorBase::ST_Force6D));
