378286d5ca64f32bb934c94eed4ee0c78070dc9a
jgvictores <jcgvicto@gmail.com>
2020-02-10 17:37:34 +0100

    cleanup: remove comments from headers

--- libraries/YarpPlugins/YarpOpenraveAnalogSensors/YarpOpenraveAnalogSensors.hpp
+++ libraries/YarpPlugins/YarpOpenraveAnalogSensors/YarpOpenraveAnalogSensors.hpp
@@ -35,56 +35,16 @@ public:
     YarpOpenraveAnalogSensors() {}
 
     // ------- DeviceDriver declarations. Implementation in DeviceDriverImageImpl.cpp -------
-    /**
-     * Open the DeviceDriver.
-     * @param config is a list of parameters for the device.
-     * Which parameters are effective for your device can vary.
-     * See \ref dev_examples "device invocation examples".
-     * If there is no example for your device,
-     * you can run the "yarpdev" program with the verbose flag
-     * set to probe what parameters the device is checking.
-     * If that fails too,
-     * you'll need to read the source code (please nag one of the
-     * yarp developers to add documentation for your device).
-     * @return true/false upon success/failure
-     */
-    virtual bool open(yarp::os::Searchable& config);
 
-    /**
-     * Close the DeviceDriver.
-     * @return true/false on success/failure.
-     */
+    virtual bool open(yarp::os::Searchable& config);
     virtual bool close();
 
     // ------- ISixAxisForceTorqueSensors declarations. Implementation in ISixAxisForceTorqueSensorsImpl.cpp -------
-    /**
-     * Get the number of six axis force torque sensors exposed by this device.
-     */
+
     virtual size_t getNrOfSixAxisForceTorqueSensors() const override;
-
-    /**
-     * Get the status of the specified sensor.
-     */
     virtual yarp::dev::MAS_status getSixAxisForceTorqueSensorStatus(size_t sens_index) const override;
-
-    /**
-     * Get the name of the specified sensor.
-     */
     virtual bool getSixAxisForceTorqueSensorName(size_t sens_index, std::string &name) const override;
-
-    /**
-     * Get the name of the frame of the specified sensor.
-     */
     virtual bool getSixAxisForceTorqueSensorFrameName(size_t sens_index, std::string &frameName) const override;
-
-    /**
-     * Get the last reading of the specified sensor.
-     *
-     * @param[in] sens_index The index of the specified sensor (should be between 0 and getNrOfSixAxisForceTorqueSensors()-1).
-     * @param[out] out The requested measure. The vector should be 6-dimensional.
-     *                 The measure is expressed in Newton for the first three elements, Newton Meters for the last three elements.
-     * @param[out] timestamp The timestamp of the requested measure, expressed in seconds.
-     */
     virtual bool getSixAxisForceTorqueSensorMeasure(size_t sens_index, yarp::sig::Vector& out, double& timestamp) const override;
 
 private:
--- libraries/YarpPlugins/YarpOpenraveRobotManager/YarpOpenraveRobotManager.hpp
+++ libraries/YarpPlugins/YarpOpenraveRobotManager/YarpOpenraveRobotManager.hpp
@@ -41,52 +41,23 @@ public:
 
     // -------- DeviceDriver declarations. Implementation in DeviceDriverImpl.cpp --------
 
-    /**
-     * Open the DeviceDriver.
-     * @param config is a list of parameters for the device.
-     * Which parameters are effective for your device can vary.
-     * See \ref dev_examples "device invocation examples".
-     * If there is no example for your device,
-     * you can run the "yarpdev" program with the verbose flag
-     * set to probe what parameters the device is checking.
-     * If that fails too,
-     * you'll need to read the source code (please nag one of the
-     * yarp developers to add documentation for your device).
-     * @return true/false upon success/failure
-     */
     virtual bool open(yarp::os::Searchable& config);
-
-    /**
-     * Close the DeviceDriver.
-     * @return true/false on success/failure.
-     */
     virtual bool close();
 
     // ------- IRobotManager declarations. Implementation in IRobotManagerImpl.cpp -------
 
-    /// @brief Robot: Move forward (use negative value for move backward). position mode [m]. Velocity mode [m/s].
     virtual bool moveForward(double value);
-
-    /// @brief Robot: Turn left (use negative value for turn left). position mode [deg]. Velocity mode [deg/s].
     virtual bool turnLeft(double value);
-
-    /// @brief Robot: Stop movement.
     virtual bool stopMovement();
-
-    /// @brief Robot camera: Tilt down (use negative value for tilt up). position mode [deg]. Velocity mode [deg/s].
     virtual bool tiltDown(double value);
-
-    /// @brief Robot camera: Pan left (use negative value for pan right). position mode [deg]. Velocity mode [deg/s].
     virtual bool panLeft(double value);
-
-    /// @brief Robot camera: Stop movement.
     virtual bool stopCameraMovement();
 
 private:
 
     robot_mode mode;
 
-    //OpenRAVE//
+    // OpenRAVE
     OpenRAVE::ControllerBasePtr pcontrol;
 
 };
