2fa43157c270a2efdca6188e53f0db7d8c319218
jgvictores <jcgvicto@gmail.com>
2020-02-10 19:34:49 +0100

    OpenraveYarpPluginLoader enable getPenv + usage example

---
+++ examples/python/openraveYarpPluginLoader-local-controlboard.py
@@ -0,0 +1,31 @@
+from openravepy import *
+import yarp
+import time
+
+try:
+    RaveInitialize()
+    env = Environment()
+    env.Load("data/lab1.env.xml")
+    env.SetViewer('qtcoin')
+
+    OpenraveYarpPluginLoader = RaveCreateModule(env,'OpenraveYarpPluginLoader')
+    penvStr = OpenraveYarpPluginLoader.SendCommand('getPenv')
+
+    options = yarp.Property()
+    options.fromString(penvStr)
+    options.put('device','YarpOpenraveControlboard')
+    options.put('robotIndex',0)
+    options.put('manipulatorIndex',0)
+    dd = yarp.PolyDriver(options)
+
+    pos = dd.viewIPositionControl()
+    pos.positionMove(1,45)
+
+    done = False
+    while not done:
+        print 'wait to reach...'
+        yarp.delay(1.0) # [s]
+        done = pos.checkMotionDone()
+
+finally:
+    RaveDestroy()
