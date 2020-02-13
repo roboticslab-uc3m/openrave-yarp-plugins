e5641dcb8e93deb796e51b5cfb0ae66f6aded24f
jgvictores <jcgvicto@gmail.com>
2020-02-13 11:25:08 +0100

    ISimulation add API doxy comments

--- libraries/YarpPlugins/ISimulation.hpp
+++ libraries/YarpPlugins/ISimulation.hpp
@@ -13,13 +13,30 @@ namespace roboticslab
  */
 class ISimulation
 {
-    public:
+public:
 
-        virtual bool step(double value) = 0;
-        virtual bool start(double value) = 0;
-        virtual bool stop() = 0;
+    /**
+     * Start the simulation (time steps will be called internally and continuously by the simulator).
+     * Simulation may default to already started, so may not be required upon initialization.
+     * @param value duration that the simulation should use within internal continuous calls to its step function
+     * @return true/false
+     */
+    virtual bool start(double value) = 0;
 
-        virtual ~ISimulation() {}
+    /**
+     * Stops the simulation.
+     * @return true/false
+     */
+    virtual bool stop() = 0;
+
+    /**
+     * Take one simulation time step (to be used when the simulation is stopped / not started).
+     * @param value duration of simulated time increment
+     * @return true/false
+     */
+    virtual bool step(double value) = 0;
+
+    virtual ~ISimulation() {}
 };
 
 }
